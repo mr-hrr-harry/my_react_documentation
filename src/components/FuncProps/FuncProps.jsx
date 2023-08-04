fa6b9169dee8a07e150b11b4ef561d1721dbf58e
M A HARISH J <mr.horror3737@gmail.com>
2023-08-04 16:31:09 +0530

    func props

---
+++ src/components/FuncProps/FuncProps.jsx
@@ -0,0 +1,12 @@
+function FuncProps(props){
+    return(
+        <div>
+            <p>Name: {props.name}</p><br />
+            <p>Age: {props.age}</p><br />
+            <p>Salary: {props.salary}</p><br />
+            <br />
+        </div>
+    )
+}
+
+export default FuncProps
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 // import WelcomeMssg from './components/WecomeMssg/WelcomeMssg';
 // import Card from './components/Card/Card';
 // import ClassComp from './components/ClassComp/ClassComp';
-import ClassProps from './components/ClassProps/ClassProps';
+// import ClassProps from './components/ClassProps/ClassProps';
+import FuncProps from './components/FuncProps/FuncProps';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,9 +17,13 @@ root.render(
     {/* <Card/> */}
     {/* <ClassComp /> */}
 
-    <ClassProps name='Harry' age={32} salary='32000' gender='Male'/>
+    {/* <ClassProps name='Harry' age={32} salary='32000' gender='Male'/>
     <ClassProps name='Sudhar' age={25} salary='64000' gender='Male'/>
-    <ClassProps name='Joe' age={27} salary='60000' gender='Male'/>
+    <ClassProps name='Joe' age={27} salary='60000' gender='Male'/> */}
+
+    {/* <FuncProps name='Harry' age='22' salary='95000'></FuncProps>
+    <FuncProps name='Sudhar' age='18' salary='100000'></FuncProps>
+    <FuncProps name='Joe' age='20' salary='98000'></FuncProps> */}
 
   </React.StrictMode>
 );
